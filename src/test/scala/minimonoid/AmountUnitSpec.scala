a1c08f866eab39625cb5fed2b67870210e243e6b
Andreas Pauley <apauley+github@gmail.com>
2015-07-03 23:17:57 +0200

    It doesn't make sense to add values of different currencies. Attempting this should result in a compile error.

--- src/test/scala/minimonoid/AmountUnitSpec.scala
+++ src/test/scala/minimonoid/AmountUnitSpec.scala
@@ -11,6 +11,10 @@ class AmountUnitSpec extends Specification {
     "pass some concrete tests" in {
       3.goats |+| 1.goat must_== 4.goats
       1.chicken |+| 7.chickens must_== 8.chickens
+
+      // It doesn't make sense to add values of different currencies.
+      // The line below must not compile.
+      // 1.chicken |+| 2.goats must_== 'donotcompile
     }
   }
 
