de23898c1bab5830fe8c1e6cfa806eecc0fde9e6
Andreas Pauley <apauley+github@gmail.com>
2015-07-03 23:14:51 +0200

    Add a chicken Amount

--- src/main/scala/minimonoid/Amount.scala
+++ src/main/scala/minimonoid/Amount.scala
@@ -2,23 +2,38 @@ package minimonoid
 
 object Amount {
 
-  implicit def amountMonoid: Monoid[Amount] = new Monoid[Amount] {
-    def id: Amount = Amount(0, Goat)
-    def op(x: Amount, y: Amount): Amount = Amount(x.amount + y.amount, id.currency)
+  implicit def goatAmountMonoid: Monoid[GoatAmount] = new Monoid[GoatAmount] {
+    def id: GoatAmount = GoatAmount(0)
+    def op(x: GoatAmount, y: GoatAmount): GoatAmount = GoatAmount(x.amount + y.amount)
   }
 
+  implicit def chickenAmountMonoid: Monoid[ChickenAmount] = new Monoid[ChickenAmount] {
+    def id: ChickenAmount = ChickenAmount(0)
+    def op(x: ChickenAmount, y: ChickenAmount): ChickenAmount = ChickenAmount(x.amount + y.amount)
+  }
 
   implicit class AmountOps(i: Int) {
-    def goats: Amount = Amount(i, Goat)
+    def goats: GoatAmount = GoatAmount(i)
     def goat = goats
 
-    def chickens: Amount = Amount(i, Chicken)
+    def chickens: ChickenAmount = ChickenAmount(i)
     def chicken = chickens
   }
 
 }
 
-case class Amount(amount: Int, currency: StableCurrency)
+trait Amount {
+  val amount: Int
+  val currency: StableCurrency
+}
+
+case class GoatAmount(amount: Int) extends Amount {
+  val currency = Goat
+}
+
+case class ChickenAmount(amount: Int) extends Amount {
+  val currency = Chicken
+}
 
 sealed trait StableCurrency
 sealed trait Livestock extends StableCurrency
