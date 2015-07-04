bc459548833f492e825be3553ac6e15882c7ffcf
Andreas Pauley <apauley+github@gmail.com>
2015-07-04 22:57:14 +0200

    Implement Amount as a Map of currency to value. Based on this code by Debasish Ghosh:
    https://gist.github.com/debasishg/efc4eeb521d2557631ae

--- src/main/scala/minimonoid/Amount.scala
+++ src/main/scala/minimonoid/Amount.scala
@@ -2,38 +2,22 @@ package minimonoid
 
 object Amount {
 
-  implicit def goatAmountMonoid: Monoid[GoatAmount] = new Monoid[GoatAmount] {
-    def id: GoatAmount = GoatAmount(0)
-    def op(x: GoatAmount, y: GoatAmount): GoatAmount = GoatAmount(x.amount + y.amount)
-  }
-
-  implicit def chickenAmountMonoid: Monoid[ChickenAmount] = new Monoid[ChickenAmount] {
-    def id: ChickenAmount = ChickenAmount(0)
-    def op(x: ChickenAmount, y: ChickenAmount): ChickenAmount = ChickenAmount(x.amount + y.amount)
+  implicit def amountMonoid: Monoid[Amount] = new Monoid[Amount] {
+    def id: Amount = Amount(Map.empty[StableCurrency, Int])
+    def op(x: Amount, y: Amount): Amount = Amount(MapUtil.unionWith(x.values, y.values)(_ + _))
   }
 
   implicit class AmountOps(i: Int) {
-    def goats: GoatAmount = GoatAmount(i)
+    def goats = Amount(Map(Goat -> i))
     def goat = goats
 
-    def chickens: ChickenAmount = ChickenAmount(i)
+    def chickens = Amount(Map(Chicken -> i))
     def chicken = chickens
   }
 
 }
 
-trait Amount {
-  val amount: Int
-  val currency: StableCurrency
-}
-
-case class GoatAmount(amount: Int) extends Amount {
-  val currency = Goat
-}
-
-case class ChickenAmount(amount: Int) extends Amount {
-  val currency = Chicken
-}
+case class Amount(values: Map[StableCurrency, Int])
 
 sealed trait StableCurrency
 sealed trait Livestock extends StableCurrency
--- src/test/scala/minimonoid/AmountUnitSpec.scala
+++ src/test/scala/minimonoid/AmountUnitSpec.scala
@@ -9,12 +9,11 @@ class AmountUnitSpec extends Specification {
 
   "The Amount monoid" should {
     "pass some concrete tests" in {
-      3.goats |+| 1.goat must_== 4.goats
+      3.goats   |+| 1.goat must_== 4.goats
       1.chicken |+| 7.chickens must_== 8.chickens
 
-      // It doesn't make sense to add values of different currencies.
-      // The line below must not compile.
-      // 1.chicken |+| 2.goats must_== 'donotcompile
+
+      5.goats |+| 9.chickens |+| 2.goats must_== Amount(Map(Chicken -> 9, Goat -> 7))
     }
   }
 
